6be8448b423ba1413578ea835a9ec11d01b55b3d
Aftermathrar <46353186+Aftermathrar@users.noreply.github.com>
2020-12-25 11:40:03 -0800

    Update SaveSlotDB.cs
    Changed lookup to pull from save file rather than rely on a list.

--- Scripts/Saving/SaveSlotDB.cs
+++ Scripts/Saving/SaveSlotDB.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ButtonGame.Saving
 {
     [CreateAssetMenu(fileName = "SaveSlotDB", menuName = "Stats/SaveSlotDB")]
     public class SaveSlotDB : ScriptableObject
     {
-        [SerializeField] List<SaveSlot> saves = new List<SaveSlot>();
+        // [SerializeField] List<SaveSlot> saves = new List<SaveSlot>();
         Dictionary<string, SaveRecord> saveLookup = null;
 
         public Dictionary<string, string> GetSaveRecord(string saveFile)
         {
-            BuildLookup();
+            if (saveLookup == null)
+            {
+                BuildLookup();
+            }
 
             if(!saveLookup.ContainsKey(saveFile))
             {
@@ -33,7 +37,10 @@ namespace ButtonGame.Saving
 
         public void AddSaveRecord(string saveFile)
         {
-            BuildLookup();
+            if(saveLookup == null)
+            {
+                BuildLookup();
+            }
 
             ISlotInfo slotInfo = GameObject.FindGameObjectWithTag("Player").GetComponent<ISlotInfo>();
             var state = (Dictionary<string, string>)slotInfo.CaptureState();
@@ -46,31 +53,38 @@ namespace ButtonGame.Saving
             newRecord.quest = state["quest"];
             newRecord.scene = state["scene"];
 
-            bool saveFound = false;
-            foreach (SaveSlot save in saves)
-            {
-                if(save.saveFile == saveFile)
-                {
-                    save.record = newRecord;
-                    saveFound = true;
-                }
-            }
-            if(!saveFound)
-            {
-                SaveSlot newSave = new SaveSlot();
-                newSave.saveFile = saveFile;
-                newSave.record = newRecord;
-                saves.Add(newSave);
-            }
+            saveLookup[saveFile] = newRecord;
         }
 
         private void BuildLookup()
         {
             saveLookup = new Dictionary<string, SaveRecord>();
 
-            foreach (SaveSlot save in saves)
+            List<string> filePaths = Directory.GetFiles(Application.persistentDataPath, "*.sav").ToList();
+            foreach (var filePath in filePaths)
             {
-                saveLookup[save.saveFile] = save.record;
+                string fileName = filePath.Replace(Application.persistentDataPath + "\\", "");
+                fileName = fileName.Replace(".sav", "");
+                Debug.Log(fileName);
+
+                var stateDict = new Dictionary<string, object>();
+                using (FileStream stream = File.Open(filePath, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    stateDict = (Dictionary<string, object>)formatter.Deserialize(stream);
+                }
+
+                stateDict = (Dictionary<string, object>)stateDict["player"];
+                var state = (Dictionary<string, string>)stateDict["ButtonGame.Attributes.PlayerInfo"];
+                SaveRecord saveRecord = new SaveRecord();
+
+                saveRecord.playerName = state["name"];
+                saveRecord.rank = state["rank"];
+                saveRecord.time = state["time"];
+                saveRecord.quest = state["quest"];
+                saveRecord.scene = state["scene"];
+
+                saveLookup[fileName] = saveRecord;
             }
         }
 
