8576b00e1a309d1921fbb493cb86fc13f103356c
Aftermathrar <46353186+Aftermathrar@users.noreply.github.com>
2021-03-24 17:30:06 -0700

    Script cleanup
    removed moved scripts

--- Scripts/Stats/IEffectProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ButtonGame.Stats
-{
-    public interface IEffectProvider
-    {
-        IEnumerable<float[]> GetStatEffectModifiers(Stat stat);
-    }
-}
